1f1729ca3b28d6110318724ed53635fd4e87e214
Matthew Fister <mfister@constantcontact.com>
2016-01-27 13:15:56 -0500

    More color functions

--- freezedraw/colors.py
+++ freezedraw/colors.py
@@ -3,23 +3,54 @@ import matplotlib.pyplot as plt
 import numpy as np
 import colorsys
 
+
 def bright_color_hsv():
     hsv = (random.uniform(0.0, 1.0), random.uniform(0.9, 1.0), random.uniform(0.9, 1.0))
     return hsv
 
+
+def dark_color_hsv():
+    hsv = (random.uniform(0.0, 1.0), random.uniform(0.0, 1.0), random.uniform(0.0, 0.5))
+    return hsv
+
+
+def near_hue_hsv(hsv):
+    h = hsv[0]+random.uniform(-0.1, 0.1)
+    if h < 0.0:
+        h = 1.0 + h
+    hsv2 = (h, hsv[1], hsv[2])
+    return hsv2
+
+
 def bright_color_rgb():
     rgb = colorsys.hsv_to_rgb(*bright_color_hsv())
-    rgb2 = [c * 255 for c in rgb]
-    print(rgb2)
-    return rgb2
+    return rgb
+
+
+def dark_color_rgb():
+    rgb = colorsys.hsv_to_rgb(*dark_color_hsv())
+    return rgb
 
 
 if __name__=='__main__':
-    h = 880
-    w = 440
-    img = np.ones((w, h, 3))*bright_color_rgb()
-    for x in range(440):
-        for y in range(880):
+    h = 100
+    w = 100
+    img = np.ones((w, h, 3))
+    for y in range(h):
+        for x in range(w):
             img[x][y] = bright_color_rgb()
-    plt.imshow(img)
+    plt.imshow(img, interpolation='nearest')
+    plt.show()
+    img2 = np.ones((w, h, 3))
+    for y in range(h):
+        for x in range(w):
+            img2[x][y] = dark_color_rgb()
+    plt.imshow(img2, interpolation='nearest')
+    plt.show()
+    img3 = np.ones((w, h, 3))
+    c = bright_color_hsv()
+    for y in range(h):
+        for x in range(w):
+            img3[x][y] = colorsys.hsv_to_rgb(*near_hue_hsv(c))
+    plt.imshow(img3, interpolation='nearest')
     plt.show()
